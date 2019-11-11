5fd0075e31d912230524f8560e40a325deec578c
clalil <cliljander@gmail.com>
2019-11-11 19:25:36 +0100

    Add test and before config for server/visit in index.js

--- cypress/integration/userCanCreateRecipe.spec.js
+++ cypress/integration/userCanCreateRecipe.spec.js
@@ -1,4 +1,36 @@
 describe('Creates a recipe', () => {
-  cy.server()
+
+  it('Successfully', () => {
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3000/v1/recipes',
+      response: 'fixture:create_recipe.json',
+      status: 200
+    })
   
+    cy.get('#create-article-form').within(() => {
+      cy.get('#create-title').type('Warm Apples')
+        .get('#create-ingredients').type('Apples, syrup')
+        .get('#create-description').type('Add syrup to apples. Heat in microwave.')
+        .get('#submit-create-form').click()
+        .get('#create-response').should('contain', 'The recipe was successfully created.')
+    })
+  })
+
+  it('Fails to', () => {
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3000/v1/recipes',
+      response: '{ message: "Unable to create recipe." }',
+      status: 400
+    })
+  
+    cy.get('#create-article-form').within(() => {
+      cy.get('#create-title').type('Warm Apples')
+        .get('#create-ingredients').type('Apples, syrup')
+        .get('#create-description').type('Add syrup to apples. Heat in microwave.')
+        .get('#submit-create-form').click()
+        .get('#create-response').should('contain', 'Unable to create recipe.')
+    })
+  })
 })
