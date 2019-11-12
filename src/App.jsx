edc5ebb2719c70745e6e2a339e785cfa7fea1213
clalil <cliljander@gmail.com>
2019-11-12 20:37:09 +0100

    Add singlerecipe component, initial state and conditional.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ListRecipes from './components/ListRecipes';
 import CreateRecipe from './components/CreateRecipe';
+import SingleRecipe from './components/SingleRecipe';
 
 class App extends Component {
   render() {
     return (
       <>
+        <SingleRecipe />
         <ListRecipes />
         <CreateRecipe />
       </>
---
+++ src/components/SingleRecipe.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Message, Container, Divider, Grid, Image } from 'semantic-ui-react'
+
+class SingleRecipe extends Component {
+
+  state = {
+    recipe: [],
+    message: null
+  }
+
+  render() {
+    let { recipe } = this.state
+
+    if (recipe !== null) {
+      showSingleRecipe = (
+        <>
+          <Container textAlign='justified' id="single-recipe">
+            <Grid columns={2}>
+              <Grid.Row>
+                <Grid.Column width={5}>
+                  <Image src={recipe.image} alt='' />
+                </Grid.Column>
+                <Grid.Column >
+                <Header as='h3' id="recipe-title">{recipe.title}</Header>
+                  <Divider />
+                  <p id="recipe-ingredients">{recipe.ingredients}</p>
+                  <p id="recipe-directions">{recipe.directions}</p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </>
+      )
+    }
+
+    return (
+      <div>
+        {showSingleRecipe}
+      </div>
+    )
+  }
+}
+
+export default SingleRecipe
