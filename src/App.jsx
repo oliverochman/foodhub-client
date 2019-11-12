24d64c8b053663205f076c9bc904515769b9ac62
clalil <cliljander@gmail.com>
2019-11-12 07:45:33 +0100

    Add browserrouter, switch and entry point.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Navbar from './components/Navbar'
+import { Switch, Route } from 'react-router-dom'
 
 class App extends Component {
+
   render() {
     return (
       <div>
+        <Switch>
+          
+        </Switch>
         <Navbar />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'semantic-ui-css/semantic.min.css';
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import 'semantic-ui-css/semantic.min.css'
+import BrowserRouter from 'react-router-dom'
 
-serviceWorker.unregister();
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'))
+
+serviceWorker.unregister()
