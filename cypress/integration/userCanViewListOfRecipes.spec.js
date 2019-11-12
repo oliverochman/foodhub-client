9b82be9ed107bfc48be212b8d66f822a1a13ba6c
Becca <beccaburns89@gmail.com>
2019-11-12 16:45:06 +0100

    WIP. Sad path for list articles.

--- cypress/integration/userCanViewListOfRecipes.spec.js
+++ cypress/integration/userCanViewListOfRecipes.spec.js
@@ -12,4 +12,16 @@ describe('FoodHub user can view a list of recipes', () => {
   it("contains a title", () => {
     cy.contains("Quiche")
   })
+  it('sees error message for no recipes', () => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3000/v1/recipes',
+      status: 200,
+      response: '{"recipes":[]}'
+    })
+    cy.visit('http://localhost:3001')
+    cy.get("#error-message")
+    cy.contains('This is an error')
+  })
 })
--- src/components/ListRecipes.jsx
+++ src/components/ListRecipes.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { getData } from '../modules/requestRecipes';
-import { Container } from 'semantic-ui-react';
 
 class ListRecipes extends Component {
   state = {
@@ -14,39 +13,42 @@ class ListRecipes extends Component {
 
   async getRecipes() {
     let result = await getData()
-    this.setState({
-      recipes: result
-    })
+
+    if (result) {
+      debugger
+      this.setState({
+        recipes: result
+      })
+    }
   }
 
   render() {
     let renderListRecipes
     const recipeData = this.state.recipes
+    let error_message
 
-    if (recipeData !== []) {
+    if (recipeData.length < 0) {
+      debugger
+      error_message = <p id="error-message">This is an error</p>
+    }
+
+    if (recipeData.length > 0) {
+      debugger
       renderListRecipes = recipeData.map(recipe => {
         return (
-          <Container>
-            <div key={recipe.id}>
-              <h1>{recipe.title}</h1>
-              <p>{recipe.ingredients}</p>
-              <p>{recipe.instructions}</p>
-            </div>
-          </Container>
-        )
-      })
-    } else {
-      return (
-        renderListRecipes = (
-          <div>
-            No recipes available.
+          <div key={recipe.id}>
+            <h1>{recipe.title}</h1>
+            <p>{recipe.ingredients}</p>
+            <p>{recipe.instructions}</p>
           </div>
         )
-      )
-    }
+      })
+    } 
+
     return (
       <>
         {renderListRecipes}
+        {error_message}
       </>
     )
   }
