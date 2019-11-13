e99b02ddb0545951716f6a7ba8d0a8a4e6137550
clalil <cliljander@gmail.com>
2019-11-13 15:40:29 +0100

    Updat integration files to match upstream latest.

--- cypress/integration/userCanNavigateSite.spec.js
+++ cypress/integration/userCanNavigateSite.spec.js
@@ -1,4 +1,13 @@
 describe('Navigating the site', () => {
+  beforeEach(() => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3000/v1/recipes',
+      response: 'fixture:recipes.json'
+    })
+    cy.visit('http://localhost:3001');
+  });
   
   it('successfully', () => {
     cy.get('#navbar').within(() => {
--- cypress/integration/userCanViewListOfRecipes.spec.js
+++ cypress/integration/userCanViewListOfRecipes.spec.js
@@ -3,16 +3,13 @@ import { getYear } from "date-fns";
 describe('FoodHub user can view a list of recipes', () => {
   beforeEach(() => {
     cy.server()
-  });
-
-  it('successfully', () => {
     cy.route({
       method: 'GET',
       url: 'http://localhost:3000/v1/recipes',
       response: 'fixture:recipes.json'
     })
     cy.visit('http://localhost:3001');
-  })
+  });
 
   it("contains recipe content", () => {
     cy.get('#list h1')
