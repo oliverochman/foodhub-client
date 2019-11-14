ed6d780a6bac513c0d41b2dd55b4cc056b62ff47
clalil <cliljander@gmail.com>
2019-11-14 13:39:25 +0100

    Completed setup of Redux.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './state/store/configureStore';
+import { verifyCredentials } from './state/actions/reduxTokenAuthConfig';
+
+const store = configureStore()
+verifyCredentials(store)
 
 ReactDOM.render(
-<BrowserRouter>
-  <App />
-</BrowserRouter>, document.getElementById('root'));
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>, 
+  document.getElementById('root'));
 
 serviceWorker.unregister()
