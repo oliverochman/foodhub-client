2196a9b379537bdc69708d9cdae742a0428f6540
clalil <cliljander@gmail.com>
2019-11-14 13:48:24 +0100

    Add a form for verifying credentials.

---
+++ src/components/verifyCredentialsForm.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Form, Button } from 'semantic-ui-react'
+
+const verifyCredentialsForm = (props) => {
+  return (
+    <>
+      <Form id="create-recipe-form"
+        onSubmit={(event) => props.submitCredentials(event)}
+      >
+        <Form.Group widths='equal'>
+          <Form.Input
+            fluid
+            label='Email'
+            placeholder='Please enter a valid email'
+            name="email"
+          />
+        </Form.Group>
+        <Form.TextArea
+          label='Ingredients'
+          placeholder='Please enter a valid password'
+          name="password"
+        />
+        <Form.TextArea
+          label='Directions'
+          placeholder='Please repeat your password'
+          name="password-confirmation"
+        />
+        <Button
+          type="submit"
+          name="submit"
+        >
+          Submit
+          </Button>
+      </Form>
+    </>
+  )
+}
+
+export default verifyCredentialsForm
