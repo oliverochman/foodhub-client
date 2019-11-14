3ab005f94ecaa139747dc7192bf33f4ba2b557dc
miyesier_kaerman <miyesier@gmail.com>
2019-11-14 14:48:08 +0100

    fix bug  backgrond image not showing.

--- cypress/integration/welcome.spec.js
+++ cypress/integration/welcome.spec.js
@@ -1,1 +1,8 @@
-describe('View the welcome page ', () => {})
+describe('View the welcome page ', () => {
+  it('should display "welcome to the foodhub page"', () => {
+    cy.get('h1')
+    cy.contains('welcome to the')
+    cy.get('h2')
+    cy.contains('foodhub');
+  })
+})
