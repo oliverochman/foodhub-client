705bf1e669654829f9fdda3377655c1dcba8545b
miyesier_kaerman <miyesier@gmail.com>
2019-11-16 14:48:17 +0100

    refacters code in landing page

--- cypress/integration/userCanViewLandingPage.spec.js
+++ cypress/integration/userCanViewLandingPage.spec.js
@@ -1,8 +1,6 @@
 describe('View the welcome page ', () => {
   it('should display "welcome to the foodhub page"', () => {
     cy.get('h1')
-    cy.contains('welcome to the')
-    cy.get('h2')
-    cy.contains('foodhub');
-  })
-})
+        .should('contain','welcome to the')
+      })
+    })
