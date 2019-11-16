50e0fe4dde2dfb3ea02d189358e702641b768c83
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-11-16 22:06:03 +0100

    Attempt to render logout message
    Co-authored-by: Clarissa Liljander <cliljander@gmail.com>

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -2,16 +2,32 @@ import React, { Component } from 'react'
 import { signOutUser } from '../state/actions/reduxTokenAuthConfig'
 import { connect } from 'react-redux'
 import { Menu, Header } from 'semantic-ui-react'
+import AlertMessage from './AlertMessage'
 
 class Logout extends Component {
+state = { message: null, error: '' }
 
   signOut = () => {
     const { signOutUser } = this.props
     signOutUser()
+    this.setState({
+      message: 'Please visit again soon!',
+      error: false
+    })
   }
 
   render() {
     const { signOut } = this
+    let {message, error} = this.state
+    let signOutMessage
+    console.log(message)
+    if (this.props.currentUser.isSignedIn === false) {
+      signOutMessage = (
+        <AlertMessage 
+        message={message}
+        error={error}
+        />
+      )}
 
     return (
       <>
@@ -20,6 +36,7 @@ class Logout extends Component {
             Log out
           </Header>
         </Menu.Item>
+        {signOutMessage}
       </>
     )
   }
