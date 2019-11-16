c3e8288fd00883659a732dbea4ce5e8d08f74780
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-11-16 15:00:11 +0100

    Add feature test for forking a recipe

---
+++ cypress/integration/userCanForkRecipe.spec.js
@@ -0,0 +1,28 @@
+describe('User forks a recipe', () => {
+  beforeEach(() => {
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3000/v1/recipes/1',
+      response: 'fixture:single_recipe.json',
+      status: 200
+    })
+  })
+
+  it('Successfully', () => {
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/v1/recipes/1/fork',
+      status: 201,
+      response: '{ "message": "The recipe was successfully forked" }'
+    })
+
+    cy.loginUser('user@mail.com', 'password')
+    cy.get('#navbar').within(() => {
+      cy.get('#nav-listrecipes')
+        .click()
+    })
+    cy.get('#recipe-1')
+      .click({ force: true })
+    cy.get('[name="fork"]').click()
+  })
+})
