dc8f60322c560053bb874f329c43f20fe487df98
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-11-17 14:11:38 +0100

    Refactores WelcomePage name and adds .should()

--- cypress/integration/userCanViewLandingPage.spec.js
+++
@@ -1,6 +0,0 @@
-describe('View the welcome page ', () => {
-  it('should display "welcome to the foodhub page"', () => {
-    cy.get('h1')
-        .should('contain','welcome to the')
-      })
-    })
---
+++ cypress/integration/userCanViewWelcomePage.spec.js
@@ -0,0 +1,8 @@
+describe('View the welcome page ', () => {
+  it('should display welcome message', () => {
+    cy.get('#welcome').within(()=>{
+      cy.get('h1').should('have.text', 'welcome to the')
+      cy.get('h2').should('have.text', 'foodhub')
+    })
+  })
+})
