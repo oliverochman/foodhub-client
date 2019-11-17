9109050bc18e7bca57b3dd577fed4cae2ec621e8
clalil <cliljander@gmail.com>
2019-11-17 09:01:06 +0100

    Clean up logout component

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -2,32 +2,16 @@ import React, { Component } from 'react'
 import { signOutUser } from '../state/actions/reduxTokenAuthConfig'
 import { connect } from 'react-redux'
 import { Menu, Header } from 'semantic-ui-react'
-import AlertMessage from './AlertMessage'
 
 class Logout extends Component {
-state = { message: null, error: '' }
 
   signOut = () => {
     const { signOutUser } = this.props
     signOutUser()
-    this.setState({
-      message: 'Please visit again soon!',
-      error: false
-    })
   }
 
   render() {
     const { signOut } = this
-    let {message, error} = this.state
-    let signOutMessage
-    console.log(message)
-    if (this.props.currentUser.isSignedIn === false) {
-      signOutMessage = (
-        <AlertMessage 
-        message={message}
-        error={error}
-        />
-      )}
 
     return (
       <>
@@ -36,7 +20,6 @@ state = { message: null, error: '' }
             Log out
           </Header>
         </Menu.Item>
-        {signOutMessage}
       </>
     )
   }
