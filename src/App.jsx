ad51fdb280a53a75a0e2aaaabe3c1f09c8ace54b
clalil <cliljander@gmail.com>
2019-11-17 08:51:14 +0100

    Remove duplicate browser routers

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 import ListRecipes from './components/ListRecipes'
 import SingleRecipe from './components/SingleRecipe'
 import Login from './components/Login'
-import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import WelcomePage from './components/WelcomePage'
 import RecipeCU from './components/RecipeCU'
 import Navbar from './components/Navbar'
 import Logout from './components/Logout'
-import { generateRequireSignInWrapper } from 'redux-token-auth' 
+import { generateRequireSignInWrapper } from 'redux-token-auth'
 
 const requireSignIn = generateRequireSignInWrapper({
   redirectPathIfNotSignedIn: '/',
@@ -17,17 +17,15 @@ class App extends Component {
   render() {
     return (
       <>
-        <BrowserRouter>
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={WelcomePage} />
-            <Route exact path='/listrecipes' component={ListRecipes} />
-            <Route exact path='/recipe/:id' component={SingleRecipe} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/logout" component={Logout} />
-            <Route exact path="/create" component={requireSignIn(RecipeCU)} />
-          </Switch>
-        </BrowserRouter>   
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={WelcomePage} />
+          <Route exact path='/listrecipes' component={ListRecipes} />
+          <Route exact path='/recipe/:id' component={SingleRecipe} />
+          <Route exact path="/logout" component={Logout} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/create" component={requireSignIn(RecipeCU)} />
+        </Switch>
       </>
     )
   }
