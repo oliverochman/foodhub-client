dd1ea77d08f91a30458f519c7e84839a2005db53
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-11-18 12:54:09 +0100

    Creates Cookbook component.

---
+++ src/components/Cookbook.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+import { Header } from "semantic-ui-react"
+import { submitRecipe, editRecipe } from "../modules/requestRecipes"
+import RecipeForm from "./RecipeForm"
+import "../css/create-recipe.css"
+import AlertMessage from './AlertMessage'
+
+class Cookbook extends Component {
+  return ()
+}
+
+
+
+
+
+
+export default Cookbook
