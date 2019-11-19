2d5ade489b7e29c2860d5fe6817525aae185d7fe
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-11-19 10:37:11 +0100

    Adds list of recipes into the cookbook component. Creates userCanSeeListOfRecipesInCookbook feature test

--- cypress/integration/userCanSaveRecipeToCookbook.spec.js
+++ cypress/integration/userCanSaveRecipeToCookbook.spec.js
@@ -2,16 +2,25 @@ describe('Save recipe to Cookbook', () => {
   beforeEach(() => {
     cy.route({
       method: 'POST',
-      url: 'http://localhost:3000/v1/favorites',
+      url: 'http://localhost:3000/v1/recipes/:recipe_id/favorite',
       response: 'fixture:favorites.json',
       status: 201
     })
   })
-  it('Successfully saves the recipe in Cookbook', () => {
-    cy.loginUser('user@mail.com', 'password')
-    cy.get('#navbar').within(() => {
-      cy.get('#nav-cookbook')
-        .click()
+  it('Successfully saves recipe to the Cookbook list', () => {
+    cy.get('#recipe-1').click({ force: true })
+    cy.get('[name="save-recipe-to-cookbook"]').click()
+    cy.get('#cookbook-recipe-form').within(() => {
+      cy.get('[name="title"]').type('Apple Pie')
+      .get('[name="ingredients"]').type('Apples, dough')
+      .get('[name="directions"]').type('Bake pie at 200')
+    const fileName = '"title": "Quiche"';
+    cy.fixture(fileName).then(fileContent => {
+      cy.get('[name="title": "Quiche"]')
+        .upload({ fileContent, fileName, mimeType: 'application/json' });
     })
+    cy.get('[name="Save to Cookbook"]').click()
   })
+  cy.get('#response-message').should('contain', 'The recipe was successfully saved')
 })
+})  
--- src/components/Cookbook.jsx
+++ src/components/Cookbook.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from "react"
-import { Header } from "semantic-ui-react"
-import { submitRecipe, editRecipe } from "../modules/requestRecipes"
-import RecipeForm from "./RecipeForm"
-import "../css/create-recipe.css"
-import AlertMessage from './AlertMessage'
+import { fetchRecipes } from "../modules/requestRecipes"
+import RecipeCard from "./RecipeCard"
 
 class Cookbook extends Component {
-  return ()
-}
+  state = {
+    recipes: []
+  }
 
+  componentDidMount() {
+    fetchRecipes().then(result => {
+      this.setState({
+        recipes: result
+      })
+    })
+  }
+  render() {
+    let renderCookbookList
+    const favoritesData = this.state.recipes
+    let message
+    if (favoritesData.length > 0) {
+      renderCookbookList = favoritesData.map(recipe => {
+        return <RecipeCard key={recipe.id} recipe={recipe} linked />
+      })
+    } else {
+      message = (
+        <Message style={{ color: "red" }}>
+          <Header as="p" id="message" style={{ color: "#4C5966" }}>
+            There are no recipes
+          </Header>
+        </Message>
+      )
+    }
+    return (
+      <>
+        {message}
+        {renderCookbookList && <Grid columns={3} id="list"><Grid.Row>{renderCookbookList}</Grid.Row></Grid>}
+      </>
+    )
+  }
+}
 export default Cookbook
