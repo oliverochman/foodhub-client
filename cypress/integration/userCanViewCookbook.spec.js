64b54f30158e9d949a65157f9d98535a52d68e81
oliverochman <oliver.ochman@hotmail.com>
2019-11-20 15:30:10 +0100

    refactor tests

--- cypress/integration/userCanViewCookbook.spec.js
+++ cypress/integration/userCanViewCookbook.spec.js
@@ -1,33 +1,22 @@
 describe('User can view cookbook', () => {
-
   beforeEach(() => {
     cy.route({
       method: 'GET',
-      url: 'http://localhost:3000/v1/recipes',
-      response: 'fixture:recipes.json',
+      url: 'http://localhost:3000/v1/favorites',
+      response: 'fixture:favorite_recipes.json',
       status: 200
     })
   })
+
   it('Successfully view My Cookbook in the Navbar', () => {
     cy.loginUser('user@mail.com', 'password')
     cy.get('#navbar')
     .within(() => {
       cy.get('#nav-cookbook').click()
       cy.get('h1').should('contain','My Cookbook')
-    }),
-
-    beforeEach(() => {
-      cy.route({
-        method: 'POST',
-        url: 'http://localhost:3000/v1/recipes/1/favorite',
-        response: 'fixture:recipes.json',
-        status: 201
-      })
     })
-    it("contains recipe content", () => {
-      cy.get('[name="recipe-2"]').within(() => {
-        cy.get('[name="recipe-title"]').should('contain', 'Chocolate Croissant')
-      })
+    cy.get('[name="recipe-2"]').within(() => {
+      cy.get('[name="recipe-title"]').should('contain', 'Chocolate Croissant')
     })
   })
 })
