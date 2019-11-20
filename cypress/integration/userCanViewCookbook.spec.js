9f51b71147a70099d215d536fcd42e50bbe19ffe
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-11-20 13:40:15 +0100

    Adds happy path of user visiting the cookbook

--- cypress/integration/userCanViewCookbook.spec.js
+++ cypress/integration/userCanViewCookbook.spec.js
@@ -8,12 +8,26 @@ describe('User can view cookbook', () => {
       status: 200
     })
   })
-  it('Successfully view recipe in the Cookbook', () => {
+  it('Successfully view My Cookbook in the Navbar', () => {
     cy.loginUser('user@mail.com', 'password')
     cy.get('#navbar')
     .within(() => {
       cy.get('#nav-cookbook').click()
       cy.get('h1').should('contain','My Cookbook')
+    }),
+
+    beforeEach(() => {
+      cy.route({
+        method: 'POST',
+        url: 'http://localhost:3000/v1/recipes/1/favorite',
+        response: 'fixture:recipes.json',
+        status: 201
+      })
     })
-  }) 
+    it("contains recipe content", () => {
+      cy.get('[name="recipe-2"]').within(() => {
+        cy.get('[name="recipe-title"]').should('contain', 'Chocolate Croissant')
+      })
+    })
+  })
 })
