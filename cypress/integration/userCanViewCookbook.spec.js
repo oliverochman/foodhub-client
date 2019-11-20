b399f184f771ce1a9e3c1afc4dc98626bc7a97d3
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-11-20 11:14:20 +0100

    Refactores userCanViewCookBook feature test adding happy path for Cookbook display in the nav bar.

--- cypress/integration/userCanViewCookbook.spec.js
+++ cypress/integration/userCanViewCookbook.spec.js
@@ -1,14 +1,20 @@
 describe('User can view cookbook', () => {
 
-  it('Successfully', () => {
+  beforeEach(() => {
     cy.route({
       method: 'GET',
-      url: 'http://localhost:3000/v1/recipes/1/favorite',
-      response: 'fixture:favorites.json',
+      url: 'http://localhost:3000/v1/recipes',
+      response: 'fixture:recipes.json',
       status: 200
     })
-  
+  })
+  it('Successfully view recipe in the Cookbook', () => {
+    cy.loginUser('user@mail.com', 'password')
+    cy.get('#navbar')
+    .within(() => {
+      cy.get('#nav-cookbook').click()
+    })
     cy.get('h1')
-      cy.should('contain', 'My Cookbook')
+    cy.should('contain', 'My Cookbook')
     })
   })
