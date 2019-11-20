0170d3005378c254c6c9e69e24353406276bd5da
clalil <cliljander@gmail.com>
2019-11-20 18:23:01 +0100

    Clean up test file, add final styling

--- cypress/integration/userCanViewOwnRecipesInProfile.spec.js
+++ cypress/integration/userCanViewOwnRecipesInProfile.spec.js
@@ -9,22 +9,19 @@ describe('Recipes created by user are displayed in the profile', () => {
     })
   })
 
-  it('successfully views own recipes ', () => {
+  it('Successfully views own recipes ', () => {
     cy.loginUser('user@mail.com', 'password')
     cy.get('#navbar')
       .within(() => {
         cy.get('#nav-profile').click()
       })
     cy.get('#profile-greeting')
-      .should('contain', 'Hello BettySpaghetti, here are the recipes you have created:')
+      .should('contain', 'Hello BettySpaghetti, welcome to your profile page')
     cy.get('[name="recipe-title"]').should('contain', 'Quiche')
     cy.get('[name="recipe-ingredients"]').should('contain', 'Eggs')
   })
-})
 
-describe('FoodHub user cannot view recipes in profile if there are none', () => {
-
-  it('sees message for no recipes', () => {
+  it('Sees message for no recipes', () => {
     cy.route({
       method: 'GET',
       url: 'http://localhost:3000/v1/recipes?user_recipe=true',
@@ -39,5 +36,12 @@ describe('FoodHub user cannot view recipes in profile if there are none', () => 
       })
     cy.get('#message')
       .should('contain', 'After you have created a recipe you can always view it here')
+  })
+
+  it('Cannot view profile page unless logged in', () => {
+    cy.get('#navbar')
+    .within(() => {
+      cy.get('#nav-profile').should('not.exist')
+    })
   })
 })
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react"
 import { fetchCurrentUsersRecipes } from "../modules/requestRecipes"
 import { Header, List, Container, Image } from "semantic-ui-react"
-import { connect } from 'react-redux'
+import { connect } from "react-redux"
 import "../css/user-profile.css"
+import { Link } from "react-router-dom"
 
 class UserProfile extends Component {
   state = {
@@ -24,11 +25,12 @@ class UserProfile extends Component {
       renderUserListOfRecipes = recipeUserData.map(recipe => {
         return (
           <List.Item key={recipe.id}>
-            <Image avatar src={recipe.image} />
-            <List.Content>
-              <List.Header name="recipe-title">Title: {recipe.title}</List.Header>
-              <List.Description name="recipe-ingredients">Ingredients: {recipe.ingredients}</List.Description>
-            </List.Content>
+              <Image avatar src={recipe.image} />
+              <List.Content style={{ fontSize: '1.2rem' }}>
+                <List.Header name="recipe-title">Title: {recipe.title}</List.Header>
+                <List.Description name="recipe-ingredients">Ingredients: {recipe.ingredients}</List.Description>
+                <Link id={`recipe-${recipe.id}`} to={`/recipe/${recipe.id}`}><List.Description className="profile-link">View recipe</List.Description></Link>
+              </List.Content>
           </List.Item>
         )
       })
@@ -42,7 +44,7 @@ class UserProfile extends Component {
 
     profileGreeting = (
       <Header as="p" id="profile-greeting" style={{ color: "#4C5966", textAlign: 'center' }}>
-        Hello {this.props.currentUser.attributes.name}, here are the recipes you have created:
+        Hello {this.props.currentUser.attributes.name}, welcome to your profile page
         </Header>
     )
 
