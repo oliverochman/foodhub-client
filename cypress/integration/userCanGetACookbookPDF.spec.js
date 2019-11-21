f161b92cb72481a2c071cc924a4baedbae853ae3
clalil <cliljander@gmail.com>
2019-11-21 10:35:57 +0100

    Update feature test

--- cypress/integration/userCanGetACookbookPDF.spec.js
+++ cypress/integration/userCanGetACookbookPDF.spec.js
@@ -2,24 +2,36 @@ describe('User can get a PDF version of the cookbook', () => {
 
   it('Successfully', () => {
     cy.route({
-      method: "POST",
-      url: "http://localhost:3000/v1/cookbooks",
-      response: { 
-        "message": "The cookbook was generated and is available for download", 
-        "url": "attachment_url"
-      },
-      status: 201
-    });
+      method: 'GET',
+      url: 'http://localhost:3000/v1/favorites',
+      response: 'fixture:favorites.json',
+      status: 200
+    }),
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/v1/cookbooks",
+        response: {
+          "message": "The cookbook was generated and is available for download",
+          "url": "attachment_url"
+        },
+        status: 201
+      });
 
     cy.loginUser('user@mail.com', 'password')
     cy.get('#navbar')
       .within(() => {
         cy.get('#nav-cookbook').click()
       })
-    cy.get('[name="recipe-title"]')
-      .should('contain', 'Quiche')
+    cy.get('h1').should('contain', 'My Cookbook')
     cy.get('[name="create-pdf"]').click()
     cy.get('#response-message')
       .should('contain', 'The cookbook was generated and is available for download at attachment_url')
+  })
+
+  it('Cannot generate a cookbook unless logged in', () => {
+    cy.get('#navbar')
+      .within(() => {
+        cy.get('#nav-cookbook').should('not.exist')
+      })
   })
 })
